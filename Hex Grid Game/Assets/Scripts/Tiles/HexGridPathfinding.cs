650dd4d83e7f8544a24961b558138b96b1a4a779
Keith Smith <keithbsmith87@gmail.com>
2019-11-07 07:40:16 -0500

    added comments to djikstra pathfinding algorithm

--- Hex Grid Game/Assets/Scripts/Tiles/HexGridPathfinding.cs
+++ Hex Grid Game/Assets/Scripts/Tiles/HexGridPathfinding.cs
@@ -45,35 +45,50 @@ public class HexGridPathfinding : MonoBehaviour
 
 	public void FindPath(HexCell destination)
 	{
+		// Sort the fringe
 		sortedPathHexes = sortedPathHexes.OrderBy(x => x.GetDjikstraCost()).ToList();
 
+		// Get the fringe element with lowest cost
 		HexCell activeCell = sortedPathHexes[0];
+		// Remove that from the fringe
 		sortedPathHexes.RemoveAt(0);
 
+		// Set the cell we're looking at to black
 		activeCell.SetDjikstraBlack();
+
+		// Look at each neighbor
 		foreach (HexCell neighbor in activeCell.GetNeighbors())
 		{
+			// If the neighbor exists (we could be at the edge of the game board) && we haven't already been there
 			if (neighbor != null && neighbor.GetDjikstraColor() != HexCell.DjikstraColor.black)
 			{
+				// Calculate the potential new cost of the neighbor
 				int newCost = activeCell.GetDjikstraCost() + neighbor.GetMoveCost();
 
+				// If the neighbor's Djikstra cost is > than the new cost + other conditions
 				if (newCost < neighbor.GetDjikstraCost() && !isFound && neighbor.GetDjikstraColor() != HexCell.DjikstraColor.black)
 				{
+					// Add the neighbor to the fringe
 					neighbor.SetDjikstraGrey();
+					// Set the neighbor's Djikstra cost
 					neighbor.SetDjikstraCost(newCost);
+					// Link the neighbor to this active cell
 					neighbor.SetParentCellID(activeCell.GetCellID());
+					// Are we there yet?
 					if (neighbor.GetCellID() == destination.GetCellID())
 					{
 						isFound = true;
 					}
 					else
 					{
+						// Add the neighbor to the finge list
 						sortedPathHexes.Add(neighbor);
 					}
 				}
 			}
 		}
 
+		// Recurse (?)
 		if (!isFound)
 		{
 			FindPath(destination);
